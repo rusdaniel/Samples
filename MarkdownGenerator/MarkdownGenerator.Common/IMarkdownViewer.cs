54bbf63f2f0e30101878c3301cc65975320c6b4f
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-05 11:55:06 +0300

    add emtpty common project
    - move interfaces to common

--- MarkdownGenerator/MarkdownGenerator.Common/IMarkdownViewer.cs
+++ MarkdownGenerator/MarkdownGenerator.Common/IMarkdownViewer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MarkdownGenerator
 {
-    interface IMarkdownViewer
+    public interface IMarkdownViewer
     {
         void DisplayDoc(string source);
     }
