e05e8198e31e7745180a57533968aae59a2d5f1a
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-06 13:53:58 +0300

    add parser class and unit tests
    - remove unnecessary code

---
+++ MarkdownGenerator/MarkdownGenerator.Parser.UnitTests/Parser.UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarkdownGenerator.Parser;
+using System.IO;
+
+namespace MarkdownGenerator.Parser.UnitTests
+{
+    [TestClass]
+    public class ParserUnitTests
+    { 
+        private static Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+
+            return stream;
+        }
+    }
+}
---
+++ MarkdownGenerator/MarkdownGenerator.Parser/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkdownGenerator.Interfaces;
+using System.IO;
+
+namespace MarkdownGenerator.Parser
+{
+    public class Parser// : IParser
+    {
+        private Stream inputData;
+
+        public Parser(Stream stream)
+        {
+            this.inputData = stream;
+        }
+
+        public string Parse()
+        {
+            StringBuilder result = new StringBuilder();
+            this.ParseHeaders(result);
+
+            return result.ToString();
+        }
+
+        private void ParseHeaders(StringBuilder result)
+        {
+        }
+    }
+}
