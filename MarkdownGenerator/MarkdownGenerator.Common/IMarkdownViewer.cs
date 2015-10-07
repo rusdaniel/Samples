671cbc30f0b0091214313aee3e3de79c28fc1206
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-07 11:05:40 +0300

    modify interfaces and remove unused usings

--- MarkdownGenerator/MarkdownGenerator.Common/IMarkdownViewer.cs
+++ MarkdownGenerator/MarkdownGenerator.Common/IMarkdownViewer.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace MarkdownGenerator.Interfaces
 {
     public interface IMarkdownViewer
     {
-        void DisplayDoc(string source);
+        void DisplayDoc(Stream source);
     }
 }
--- MarkdownGenerator/MarkdownGenerator.Common/IParser.cs
+++ MarkdownGenerator/MarkdownGenerator.Common/IParser.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace MarkdownGenerator.Interfaces
 {
     public interface IParser
     {
-        bool TryParse(string text, ref string result);
+        Stream Parse();
     }
 }
