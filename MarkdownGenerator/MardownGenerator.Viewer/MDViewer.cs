966f714ffec580c639fd28d09a41b0a9cbc65ca7
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-07 13:49:33 +0300

    implement basic viewer

---
+++ MarkdownGenerator/MardownGenerator.Viewer/MDViewer.cs
@@ -0,0 +1,32 @@
+﻿using MarkdownGenerator.Interfaces;
+using System.Diagnostics;
+using System.IO;
+
+namespace MardownGenerator.Viewer
+{
+    public class MDViewer : IMarkdownViewer
+    {
+        private const string fileName = "markdownDoc.html";
+        private string filePath;
+
+        public MDViewer()
+        {
+            this.filePath = Path.Combine(Path.GetTempPath(), fileName);
+        }
+
+        public void DisplayDoc(Stream source)
+        {
+            this.SaveDataToFile(source);
+            Process.Start(this.filePath);
+        }
+
+        private void SaveDataToFile(Stream data)
+        {
+            using (var fs = File.Create(this.filePath))
+            {
+                data.Seek(0, SeekOrigin.Begin);
+                data.CopyTo(fs);
+            }
+        }
+    }
+}
