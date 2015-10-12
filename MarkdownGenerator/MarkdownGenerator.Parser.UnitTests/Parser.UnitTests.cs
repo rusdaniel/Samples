e3736fa7803a6a6dbcd943bce0f2ea57ba31e10b
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-12 13:56:53 +0300

    modify the parser unit tests

--- MarkdownGenerator/MarkdownGenerator.Parser.UnitTests/Parser.UnitTests.cs
+++ MarkdownGenerator/MarkdownGenerator.Parser.UnitTests/Parser.UnitTests.cs
@@ -1,13 +1,48 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MarkdownGenerator.Parser;
-using System.IO;
-
-namespace MarkdownGenerator.Parser.UnitTests
+﻿namespace MarkdownGenerator.Parser.UnitTests
 {
+    using MarkdownGenerator.Common.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization.Formatters.Binary;
+
     [TestClass]
     public class ParserUnitTests
-    { 
+    {
+
+        [TestMethod]
+        public void Given_TextContainingHeader1_When_Parsed_Then_IsCorrectlyFormatted()
+        {
+            var inputText = "# Header 1#\n";
+            var parser = new ParserStateMachine();
+            var outputStream = parser.Parse(GenerateStreamFromString(inputText));
+
+            var mdDoc = StreamToMdDoc(outputStream);
+
+            Assert.IsTrue(mdDoc.GetHeaders().Count == 1, "The number of containing headers is incorrect");
+            Assert.IsTrue(string.Compare(
+                    @"<h1>Header 1</h1>",
+                    mdDoc.GetHeaders().First().ToString(),
+                    StringComparison.InvariantCultureIgnoreCase) == 0);
+        }
+
+        [TestMethod]
+        public void Given_TextContainingHeader1WithNewLine_When_Parsed_Then_IsCorrectlyFormatted()
+        {
+            var inputText = "# Header 1\n";
+            var parser = new ParserStateMachine();
+            var outputStream = parser.Parse(GenerateStreamFromString(inputText));
+
+            var mdDoc = StreamToMdDoc(outputStream);
+
+            Assert.IsTrue(mdDoc.GetHeaders().Count == 1, "The number of containing headers is incorrect");
+            Assert.IsTrue(string.Compare(
+                    @"<h1>Header 1</h1>",
+                    mdDoc.GetHeaders().First().ToString(),
+                    StringComparison.InvariantCultureIgnoreCase) == 0);
+        }
+
         private static Stream GenerateStreamFromString(string s)
         {
             MemoryStream stream = new MemoryStream();
@@ -17,6 +52,15 @@ namespace MarkdownGenerator.Parser.UnitTests
             stream.Position = 0;
 
             return stream;
+        }
+
+        private static MdDoc StreamToMdDoc(Stream data)
+        {
+            var formatter = new BinaryFormatter();
+            data.Seek(0, SeekOrigin.Begin);
+            var result = formatter.Deserialize(data);
+
+            return (MdDoc)result;
         }
     }
 }
