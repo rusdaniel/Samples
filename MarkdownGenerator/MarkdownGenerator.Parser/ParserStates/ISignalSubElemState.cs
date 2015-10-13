c5d3508985582a824aed8bf7311c0450ea3ed548
RusDaniel <rus.daniel.eugen@gmail.com>
2015-10-13 11:44:50 +0300

    add new interface for signaling sub elements completion

---
+++ MarkdownGenerator/MarkdownGenerator.Parser/ParserStates/ISignalSubElemState.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MarkdownGenerator.ParserStates
+{
+    using MarkdownGenerator.Common.Data;
+
+    public interface ISignalSubElemState : ISignalState
+    {
+        void OnSubElementCompleted(MdElement subElem);
+    }
+}
